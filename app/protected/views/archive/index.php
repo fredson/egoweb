ce052b76a0cf7fa3b2fc5aff3ea06d07d93be5f5
fredson <f@brndnstn.de>
2018-08-08 10:39:34 +0200

    fixed links in js in protected/views/archive/index.php

--- app/protected/views/archive/index.php
+++ app/protected/views/archive/index.php
@@ -1,3 +1,10 @@
+<!-- defining urls for restore, delete --> 
+<?php
+Yii::app()->clientScript->registerScript('helpers', '
+        deleteUrl = '.CJSON::encode(Yii::app()->createUrl('/authoring/delete/'.$data->id)).';
+        restoreUrl = '.CJSON::encode(Yii::app()->createUrl('/authoring/restore/'.$data->id)).';
+');?>
+
 <div class="view" style="width:360px;float:left;margin-right:30px">
 	<h2>Single Session Studies</h2>
 	<?php foreach($single as $data): ?>
@@ -7,20 +14,21 @@
 			"Restore",
 			array(
 				"class"=>"btn btn-primary btn-sm pull-right",
-				"onclick"=>"js:document.location.href='/archive/restore/".$data->id."'"
+				"onclick"=>"js:document.location.href=restoreUrl"
 			)
 		); ?>
 		<?php echo CHtml::button(
 			"Delete",
 			array(
 				"class"=>"btn btn-danger btn-sm pull-right",
-				"onclick"=>"js:if(confirm('Are you sure you want to delete this study?')){document.location.href='/authoring/delete/".$data->id. "'}"
+				"onclick"=>"js:if(confirm('Are you sure you want to delete this study?')){document.location.href=deleteUrl}"
 			)
 		); ?>
 
 
 	<?php endforeach; ?>
 </div>
+
 
 <div class="view" style="width:360px;float:left;margin-right:30px">
 	<h2>Multi Session Studies</h2>
@@ -30,14 +38,14 @@
 			"Restore",
 			array(
 				"class"=>"btn btn-primary btn-sm pull-right",
-				"onclick"=>"js:document.location.href='/archive/restore/".$data->id."'"
+				"onclick"=>"js:document.location.href=restoreUrl"
 			)
 		); ?>
 		<?php echo CHtml::button(
 			"Delete",
 			array(
 				"class"=>"btn btn-danger btn-sm pull-right",
-				"onclick"=>"js:if(confirm('Are you sure you want to delete this study?')){document.location.href='/authoring/delete/".$data->id. "'}"
+				"onclick"=>"js:if(confirm('Are you sure you want to delete this study?')){document.location.href=deleteUrl}"
 			)
 		); ?>
 
